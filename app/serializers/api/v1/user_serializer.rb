48804cc6465ab2224021c33697fd3061961fedf2
Dane Thurber <dane.thurber@gmail.com>
2014-02-27 09:43:30 -0700

    config users serialzer with helpful methods

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,3 +1,11 @@
 class API::V1::UserSerializer < API::BaseSerializer
-  attributes :id
+  attributes :id, :name, :email, :href
+
+  def name
+    "#{object.first_name} #{object.last_name}"
+  end
+
+  def href
+    api_v1_user_url object
+  end
 end
